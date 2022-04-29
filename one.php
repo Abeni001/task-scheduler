b26317eac7284d42dc34ef76b98aeaffb10325da
Abeni <getnetabenezar@gmail.com>
2022-04-29 13:44:43 -0200

    working with real db

--- one.php
+++ one.php
@@ -1,7 +1,7 @@
 <?php include 'header.php';?>
 
 <?php
-$add=$date='hey';
+$add=$date='';
 $adderr=$dateerr='';
 
 if(isset($_POST['submit'])){
@@ -9,41 +9,42 @@ if(isset($_POST['submit'])){
   if(empty($_POST['add'])){
     $adderr ="task is required";
   }else{
-    $add = filter_input(INPUT_POST,$add,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $add = filter_input(INPUT_POST,'add',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   }
 
   //validate date
   if(empty($_POST['date'])){
     $dateerr ="task is required";
   }else{
-    $date = filter_input(INPUT_POST,$date,FILTER_SANITIZE_SPECIAL_CHARS);
+    $date = filter_input(INPUT_POST,'date',FILTER_SANITIZE_SPECIAL_CHARS);
   }
 
-//   if(empty($adderr) && empty($dateerr)){
-//  $sql = 'INSERT INTO task_schedule (add) VALUES("$add")';
+  if(empty($adderr) && empty( $dateerr)){
 
-//  if(mysqli_query($con,$sql)){
-//    echo "nice";
-//  }else{
-//    echo 'not good'. mysqli_error($con);
-//  }
+//     $sq ="INSERT INTO task_shedule('add') VALUES ('$add')";
+//     if(mysqli_query($con,$sq)){
+// echo "success";
 
-//   }
+//     }else{
+//       echo "wrong" . mysqli_error($con);
+//     }
+  $sql = "INSERT INTO task_shedule (add)
+VALUES ('$add')";
+
+if ($con->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $con->error;
 }
-$tasks = [
-    [
-      'id'=>'1',
-      'add'=>"task1"
-    ],
-    [
-      'id'=>'2',
-      'add'=>"task2"
-    ],
-    [
-      'id'=>'3',
-      'add'=>"task3"
-    ],
-];
+
+$con->close();
+}
+
+
+}
+$sql = 'SELECT * FROM task_shedule';
+$resut = mysqli_query($con,$sql);
+$tasks = mysqli_fetch_all($resut,MYSQLI_ASSOC);
 ?>
   
   <div class="container">
@@ -51,7 +52,7 @@ $tasks = [
     <h2 class="title">Add Tasks</h2>
     <form  action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST" id="addForm" class="form-inline mb-3">
       <input type="text" class="form-control mr-2 <?php echo !$adderr ? :'invalid';?>" id="item" name="add" placeholder="add task"><br>
-      <input type="date" class="form-control mr-2 <?php echo !$dateerr ? :'invalid';?>" id="item2" name="date">
+      <input type="datetime" class="form-control mr-2 <?php echo !$dateerr ? :'invalid';?>" id="item2" name="date">
 
       <input type="submit" class="bb" value="submit" name="submit">
     </form>
